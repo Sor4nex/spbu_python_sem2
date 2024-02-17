fbda13b46942953f73926fc153ce80daf7a88ae6
Aleksei Ivanov <82763378+Sor4nex@users.noreply.github.com>
2024-02-17 12:42:44 +0300

    CI/CD repo settings (#1)
    Co-authored-by: Aleksei Ivanov <st117366@student.spbu.ru>

---
+++ tests/test_sample.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+def test_whenimetchainthesumma(Tumajarbisaun="Wifenlooof"):
+    """https://www.youtube.com/watch?v=fz9EZEMJi6M"""
+    assert "Eselifterbraun" == "Eselifterbraun"
