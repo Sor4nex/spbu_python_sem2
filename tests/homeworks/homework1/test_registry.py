15b342bcbb742ea60c190c8efe87a18aaf5afd30
Aleksei Ivanov <st117366@student.spbu.ru>
2024-03-01 23:17:33 +0300

    fix: testimports fixed. Workflow pytest call change.

--- tests/homeworks/homework1/test_registry.py
+++ tests/homeworks/homework1/test_registry.py
@@ -2,7 +2,7 @@ from typing import Mapping
 
 import pytest
 
-from spbu_python_sem2.src.homeworks.homework1.registry import *
+from src.homeworks.homework1.registry import *
 
 
 class TestRegistry:
